23cb48717b8dd66a7f9e97273f753f82ecfaec89
Samuel Just <sjust@redhat.com>
2020-09-17 15:56:20 -0700

    crimson/os/seastore/lba_manager: add get_physical_extent_if_live
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/os/seastore/segment_cleaner.h
+++ src/crimson/os/seastore/segment_cleaner.h
@@ -250,6 +250,22 @@ public:
     virtual rewrite_extent_ret rewrite_extent(
       Transaction &t,
       CachedExtentRef extent) = 0;
+
+    /**
+     * get_extent_if_live
+     *
+     * See LBAManager::get_extent_if_live.
+     */
+    using get_extent_if_live_ertr = crimson::errorator<
+      crimson::ct_error::input_output_error>;
+    using get_extent_if_live_ret = get_extent_if_live_ertr::future<
+      CachedExtentRef>;
+    virtual get_extent_if_live_ret get_extent_if_live(
+      Transaction &t,
+      extent_types_t type,
+      paddr_t addr,
+      laddr_t laddr,
+      segment_off_t len) = 0;
   };
 
 private:
