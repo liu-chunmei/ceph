8d600e3232f6dcb807a64533e879e845418e8796
chunmei-liu <chunmei.liu@intel.com>
2022-02-19 10:56:15 -0800

    crimson/seastore: make fiemap return errorator future
    Signed-off-by: chunmei-liu <chunmei.liu@intel.com>

--- src/crimson/os/seastore/seastore.cc
+++ src/crimson/os/seastore/seastore.cc
@@ -930,7 +930,8 @@ SeaStore::_fiemap_ret SeaStore::_fiemap(
       len);
   });
 }
-seastar::future<std::map<uint64_t, uint64_t>> SeaStore::fiemap(
+
+SeaStore::read_errorator::future<std::map<uint64_t, uint64_t>> SeaStore::fiemap(
   CollectionRef ch,
   const ghobject_t& oid,
   uint64_t off,
@@ -954,9 +955,6 @@ seastar::future<std::map<uint64_t, uint64_t>> SeaStore::fiemap(
       size - off:
       std::min(size - off, len);
     return _fiemap(t, onode, off, adjust_len);
-  }).handle_error(
-    crimson::ct_error::assert_all{
-      "Invalid error in SeaStore::fiemap"
   });
 }
 
